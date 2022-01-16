66fd9415a6159d88cb6948e0ebcb12c4c3682420
sanju8892 <97700485+sanju8892@users.noreply.github.com>
2022-01-17 02:11:34 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -3,6 +3,6 @@ class Simple{
      System.out.println("Hello Java"); 
      System.out.println("making for auto build " );  
      System.out.println("job should run automatically as we have did the setting in poll scm");  
-        
+     System.out.println("this is auto build ");   
     }  
 }  
